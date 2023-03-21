b2d31760c88c7fa73309d8b1bb43a3b657795dc4
ronil patel <ronilpatel1001@gmail.com>
2023-03-21 18:09:25 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ronil",
+    address: "122 blue street",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newobj = {...obj };
+    newobj[key] = value;
+    return newobj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj, key, value) {
+    const newobj = {...obj };
+    newobj[key] = value;
+    return newobj;
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
